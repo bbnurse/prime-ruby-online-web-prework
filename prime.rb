591f10a3da1f60ffe5b0f3a6f3255d48683ffbd5
blanca bell <b.p.bell15@gmail.com>
2019-01-01 21:52:13 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,14 +1,13 @@
 # Add  code here!
 def prime?(number)
   start = 2
-  if number >1 
+  if number >1
     range = (start..number-1).to.a
     range.none? do |num_to_test|
 
       number % num_to_test == 0
     end
-  else 
+  else
     false
   end
-end 
-  
+end
